5c20bcae9a90fab2dc9b8288f20b1be132cc8477
abdulkalam-011 <abdulkalam.buss@gmail.com>
2025-12-04 13:09:01 +0530

    added utils for apierror apiresponse and aynch data handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,35 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 const app = express();
+
+
+// cors middleware 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}))
+
+// to accept json data 
+app.use(express.json({limit:"16kb"}))
+
+// to accept data from url
+app.use(express.urlencoded({extended:true}))
+
+// serves static files
+app.use(express.static("public"))
+
+// cookie parser middleware to perform cors operations on browser on cookies
+app.use(cookieParser()) 
+
+app.use(morgan('dev'));
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
