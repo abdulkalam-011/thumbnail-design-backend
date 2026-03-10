9d663fbdb27a25543f20870e8792d8a665b85f8d
abdulkalam-011 <abdulkalam.buss@gmail.com>
2026-03-10 21:47:38 +0530

    refactor: simplify get user by ID route by using existing controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
+
+
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { upload } from "../middlewares/multer.middleware.js";
-import { registerUser } from "../controllers/auth.controllers.js";
+import { getUserById } from "../controllers/user.controllers.js";
 
 //  route : /api/v1/user/
 //  Purpose : Create a new user
@@ -23,21 +24,11 @@ router.get("/", async (req, res) => {
   }
 });
 
+
 // Get user by ID
-router.get("/:id", async (req, res) => {
-  const userId = req.params.id;
-  try {
-    const user = await User.findById(userId).select("-password -refreshToken");
-    if (!user) {
-      return res.status(404).json(new ApiError(404, "User not found"));
-    }
-    return res
-      .status(200)
-      .json(new ApiResponse(200, "User fetched successfully", user));
-  } catch (error) {
-    return res.status(500).json(new ApiError(500, "Server error"));
-  }
-});
+router.get("/:id", getUserById);
+
+
 router.put("/:id", async (req, res) => {
   const userId = req.params.id;
   const updateData = req.body;
