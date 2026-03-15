827d9fedec01755ae6f008c1e0802cc697c75297
abdulkalam-011 <abdulkalam.buss@gmail.com>
2026-03-15 21:42:12 +0530

    fix: commented unnecessary code

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,56 +11,53 @@ import {
 } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
-//  route : /api/v1/user/
-//  Purpose : Create a new user
-
 // Get all users
-// router.get("/", );
-router.route("/").get(authenticateUser, authorizeRoles("user"), getAllUsers);
+// router.get("/");
+router.route("/").get(authenticateUser, authorizeRoles("admin"), getAllUsers);
 
 // Get user by ID
 // router.get("/:id", getUserById);
-router.route("/:id").get(getUserById);
+router.route("/:id").get(authenticateUser,authorizeRoles("admin"),getUserById);
 
 
-router.put("/:id", async (req, res) => {
-  const userId = req.params.id;
-  const updateData = req.body;
-  try {
-    const user = await User.findByIdAndUpdate(userId, updateData, {
-      new: true,
-    }).select("-password -refreshToken");
+// router.put("/:id", async (req, res) => {
+//   const userId = req.params.id;
+//   const updateData = req.body;
+//   try {
+//     const user = await User.findByIdAndUpdate(userId, updateData, {
+//       new: true,
+//     }).select("-password -refreshToken");
 
-    if (!user) {
-      return res.status(404).json(new ApiError(404, "User not found"));
-    }
-    return res
-      .status(200)
-      .json(new ApiResponse(200, "User updated successfully", user));
-  } catch (error) {
-    return res.status(500).json(new ApiError(500, "Server error"));
-  }
-});
+//     if (!user) {
+//       return res.status(404).json(new ApiError(404, "User not found"));
+//     }
+//     return res
+//       .status(200)
+//       .json(new ApiResponse(200, "User updated successfully", user));
+//   } catch (error) {
+//     return res.status(500).json(new ApiError(500, "Server error"));
+//   }
+// });
 
-router.post("/:id/avatar", async (req, res) => {
-  const userId = req.params.id;
-  const { profilePicture } = req.body;
-  try {
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { profilePicture },
-      { new: true }
-    ).select("-password -refreshToken");
-    if (!user) {
-      return res.status(404).json(new ApiError(404, "User not found"));
-    }
-    return res
-      .status(200)
-      .json(new ApiResponse(200, "User avatar updated successfully", user));
-  } catch (error) {
-    return res.status(500).json(new ApiError(500, "Server error"));
-  }
-});
+// router.post("/:id/avatar", async (req, res) => {
+//   const userId = req.params.id;
+//   const { profilePicture } = req.body;
+//   try {
+//     const user = await User.findByIdAndUpdate(
+//       userId,
+//       { profilePicture },
+//       { new: true }
+//     ).select("-password -refreshToken");
+//     if (!user) {
+//       return res.status(404).json(new ApiError(404, "User not found"));
+//     }
+//     return res
+//       .status(200)
+//       .json(new ApiResponse(200, "User avatar updated successfully", user));
+//   } catch (error) {
+//     return res.status(500).json(new ApiError(500, "Server error"));
+//   }
+// });
 
 router.route("/update-profile-picture").patch(authenticateUser,upload.single("profilePicture"),updateUserProfilePicture)
 router.route("/update-user").patch(authenticateUser,updateUserDetails)
