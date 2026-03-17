1d04bd9a80a11a55bae25b9d9ec385915ad0727b
abdulkalam-011 <abdulkalam.buss@gmail.com>
2026-03-17 13:02:51 +0530

    refactor: remove unused work routes and consolidate upload route

--- src/routes/workRoutes.js
+++ src/routes/workRoutes.js
@@ -1,38 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { Work } from '../models/work.models.js';
-import {ApiError} from '../utils/ApiError.js';
-import {ApiResponse} from '../utils/ApiResponse.js';
-import e from 'express';
 
-//  route :/api/v1/auth/login
+import { uploadWork } from '../controllers/work.controllers.js';
 
-router.get("/",async(req,res,)=>{
 
-  try {
-      const works = await  Work.find();
-      return res.status(200).json(new ApiResponse(200, works, works));
-  } catch (error) {
-    console.log('work route err :', error);
-  }
-})
 
-router.post("/",async(req,res,)=>{
 
-  try { 
-
-      const { title, description } = req.body;
-      if(!title){
-        return res.status(400).json(new ApiError(400, "title is required"));
-      } 
-      const newWork = new Work({
-        title,
-        description});
-
-      const savedWork = await newWork.save();
-      return res.status(201).json(new ApiResponse(201, savedWork, "Work created successfully"));
-  }catch (error) {
-    console.log('work route err :', error);
-  }}) 
+router.route("/upload-work").post(uploadWork)
 
 export {router as workRoutes};
