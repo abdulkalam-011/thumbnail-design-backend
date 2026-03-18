25048aedfd5f00ba78e0fa9fb35d2b9e1c43a6f4
abdulkalam-011 <abdulkalam.buss@gmail.com>
2026-03-18 12:53:57 +0530

    refactor: integrate YoutubeCron invocation into server startup process

--- src/index.js
+++ src/index.js
@@ -3,37 +3,27 @@ import app from "./app.js";
 import connectDB from "./config/db.js";
 import connectCloudinary from "./config/cloudinary.js";
 import { getCategoryName, getVideoInfo } from "./utils/getYoutubeVideo.js";
-
-
-// import "./cron/youtube.cron.js"
+import { YoutubeCron } from "./cron/youtube.cron.js";
 
 // dotenv.config({
 //   path: "./.env",
 // });
 
-const PORT = process.env.PORT || 4000;
-
-// const video = await getVideoInfo("https://www.youtube.com/watch?v=dESIGVxSSCE")
-// console.log("Video:",  video.snippet)
-
-// const category = await getCategoryName(video.snippet.categoryId)
-// console.log("CATEGORY: ",category)
-
-
-
 connectDB()
-.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on  http://localhost:${PORT}`);
+  .then(() => {
+    YoutubeCron()
+    const PORT = process.env.PORT || 4000
+    app.listen(PORT, () => {
+      console.log(`Server is running on  http://localhost:${PORT}`);
+    });
+    app.on("error", (err) => {
+      console.error(" internal Server error:", err);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+    process.exit(1);
   });
-  app.on("error", (err) => {
-    console.error(" internal Server error:", err);
-  });
-})
-.catch((err) => {
-  console.error("Failed to connect to the database:", err);
-  process.exit(1);
-});
-
 
 connectCloudinary();
+
