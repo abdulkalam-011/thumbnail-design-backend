010ab96b865eb656521ec8ffa00d9b1791d0c832
abdulkalam-011 <abdulkalam.buss@gmail.com>
2026-03-18 21:56:56 +0530

    refactor: improve video ID extraction logic and clean up category mapping

--- src/utils/getYoutubeVideo.js
+++ src/utils/getYoutubeVideo.js
@@ -1,26 +1,46 @@
 import axios from "axios";
 
-
-
 const categoryMap = {
-  "1": "Film & Animation",
-  "2": "Autos & Vehicles",
-  "10": "Music",
-  "15": "Pets & Animals",
-  "17": "Sports",
-  "20": "Gaming",
-  "22": "People & Blogs",
-  "23": "Comedy",
-  "24": "Entertainment",
-  "25": "News & Politics",
-  "26": "Howto & Style",
-  "27": "Education",
-  "28": "Science & Technology"
+  1: "Film & Animation",
+  2: "Autos & Vehicles",
+  10: "Music",
+  15: "Pets & Animals",
+  17: "Sports",
+  20: "Gaming",
+  22: "People & Blogs",
+  23: "Comedy",
+  24: "Entertainment",
+  25: "News & Politics",
+  26: "Howto & Style",
+  27: "Education",
+  28: "Science & Technology",
 };
 
+const exractVideoId = (url) => {
+  try {
+    const parseUrl = new URL(url);
+    if (parseUrl.hostname == "youtu.be") {
+      return parseUrl.pathname.slice(1);
+    }
+    const v = parseUrl.searchParams.get("v");
+    if (v) return v;
+
+    if (parseUrl.pathname.includes("/shorts/")) {
+      return parseUrl.pathname.split("/shorts/")[1].split("?")[0];
+    }
+
+    if (parseUrl.pathname.includes("/embed/")) {
+      return parseUrl.pathname.split("/embed/")[1].split("?")[0];
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
 async function getVideoInfo(videoUrl) {
-  const videoId = new URL(videoUrl).searchParams.get("v");
+  const videoId = exractVideoId(videoUrl);
   try {
     const res = await fetch(
       `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoId}&key=${process.env.YOUTUBE_API_KEY}`
@@ -33,11 +53,9 @@ async function getVideoInfo(videoUrl) {
   }
 }
 
-
- function getCategoryName(categoryId) {
-  return categoryMap[categoryId] || undefined
+function getCategoryName(categoryId) {
+  return categoryMap[categoryId] || undefined;
 }
-
 
 async function getChannelInfo(channelId) {
   try {
@@ -47,15 +65,14 @@ async function getChannelInfo(channelId) {
         params: {
           part: "snippet,statistics",
           id: channelId,
-          key: process.env.YOUTUBE_API_KEY
-        }
+          key: process.env.YOUTUBE_API_KEY,
+        },
       }
     );
-  console.log("CHANNEL RAW RESPONSE:", data);
     if (!data.items || data.items.length === 0) {
       return null;
     }
-    
+
     const channel = data.items[0];
 
     return {
@@ -64,13 +81,12 @@ async function getChannelInfo(channelId) {
       thumbnails: channel.snippet.thumbnails,
       subscribers: channel.statistics.subscriberCount,
       totalViews: channel.statistics.viewCount,
-      totalVideos: channel.statistics.videoCount
+      totalVideos: channel.statistics.videoCount,
     };
-
   } catch (error) {
     console.log("CHANNEL FETCH ERROR:", error);
     return null;
   }
 }
 
-export {getVideoInfo, getCategoryName, getChannelInfo}
+export { getVideoInfo, getCategoryName, getChannelInfo };
